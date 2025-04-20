5e1b4fefbb144a98f1ac908ead471276f9e08e22
Saagar-lab <23td0091@rgcet.edu.in>
2025-04-21 00:52:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export  default defineConfig({
-  base: '/treasure-hunt/', // 👈 update this!
+export default defineConfig({
   plugins: [react()],
+ 
 })
